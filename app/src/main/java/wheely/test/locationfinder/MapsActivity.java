bf766b040d76672960d006799c55c65479e8b480
shepakin <shepakin@evast.ru>
2016-09-08 17:59:21 +0500

    ADD testing socket io

--- app/src/main/java/wheely/test/locationfinder/MapsActivity.java
+++ app/src/main/java/wheely/test/locationfinder/MapsActivity.java
@@ -18,6 +18,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        Api api = new Api();
+        api.createSocket();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
